09bdd7ceb7f14547fbaa5b35c66deb5acac0ce5b
Sivasankar. S <96108681+Sivsankars@users.noreply.github.com>
2022-05-11 16:05:50 +0530

    Update Result.java

--- co3/Result.java
+++ co3/Result.java
@@ -1,55 +1,32 @@
-package courseoutcome1;
+
 import java.util.*;
-class student{
-	String name;
-	student(String sname){
-		name=sname;
-	}
+interface student{
+	String name="shibili";
+	int mark1=100;
+	
 }
-class sports extends student{
-	String sport;
-	String grade;
-	static int j=1;
-	sports(String sname,String ssports,String sgrade){
-		super(sname);
-		sport=ssports;
-		grade=sgrade;
-	}
+interface sports{
+	int sportm=101;
+	String sport="football";
+	
+	
 }
-public class Result extends sports {
-	Result(String sname,String ssports,String sgrade){
-		super(sname,ssports,sgrade);
-	}
+public class Result implements student,sports{
+	
 	void display() {
-		System.out.print(j+"\t"+name+"\t"+sport+"\t");
-		System.out.print(grade);
+		System.out.print(" name: "+name+"\n acadamic mark: "+mark1+"\n sport :"+sport+"\n sport mark :"+sportm);
+		
 	}
 
 	public static void main(String args[]) {
-		Scanner in=new Scanner(System.in);
-		int n=in.nextInt();
-		Result[] res=new Result[10];
-		for(int i=1;i<=n;i++) {
-			System.out.println("Enter the name of the student ");
-			String a=in.nextLine();
-			a=in.nextLine();
-			System.out.println("Enter the sport category of the student ");
-			String b=in.nextLine();
-			System.out.println("Enter the grade given to the student ");
-			String c=in.nextLine();
-			res[i]=new Result(a,b,c);
-		}
+		
+		Result res=new Result();
+		
 		System.out.println("-------student  Details--------");
-		System.out.println("NO	NAME	SPORT	GRADE");
-		for(int i=1;i<=n;i++) {
-			System.out.println();
-			res[i].display();
-			j++;
-		}
+		res.display();
 		
 		
 
 	}
 
 }
-
